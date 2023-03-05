e9f66ddf0bbb3a368e09d4388de906a9cd3c381b
alex289 <mccr4fthd@gmail.com>
2023-03-05 21:47:33 +0100

    Initial commit

---
+++ CleanArchitecture.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+        return "test";
+    }
+}
