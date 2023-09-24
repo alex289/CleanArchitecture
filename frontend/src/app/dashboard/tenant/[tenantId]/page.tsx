3342fb1105e33d9cc5fa74597ba070d5c16405b2
Alexander Konietzko <alexander.konietzko@netgo.de>
2023-09-24 14:07:23 +0200

    feat: Delete tenants and users

--- frontend/src/app/dashboard/tenant/[tenantId]/page.tsx
+++ frontend/src/app/dashboard/tenant/[tenantId]/page.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import { redirect } from 'next/navigation';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
 import UserTable from '@/components/tables/user-table';
 import { useAPI } from '@/lib/use-api';
 
@@ -20,12 +35,51 @@ export default function TenantPage({
     return <div className="m-4">Error loading tenant :(</div>;
   }
 
+  async function deleteTenant(event: React.SyntheticEvent) {
+    event.preventDefault();
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/tenant/${params.tenantId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('auth_token')}`,
+        },
+      },
+    );
+
+    if (res.ok) {
+      redirect('/dashboard');
+    }
+  }
+
   return (
     <main>
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-2xl font-bold tracking-tight">
           {tenant.data.data?.name}
         </h2>
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <Button variant="destructive">Delete</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                tenant and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={deleteTenant}>
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
       <div className="container mx-auto py-10">
         <UserTable data={tenant.data.data?.users ?? []} />
--- frontend/src/app/dashboard/user/[userId]/page.tsx
+++ frontend/src/app/dashboard/user/[userId]/page.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import { redirect } from 'next/navigation';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
 import { useAPI } from '@/lib/use-api';
 
 import { UserRole } from '@/types/user-role.enum';
@@ -21,12 +36,52 @@ export default function UserPage({ params }: { params: { userId: string } }) {
   const role = UserRole[tenant.data.data?.role!];
   const status = UserStatus[tenant.data.data?.status!];
 
+  async function deleteUser(event: React.SyntheticEvent) {
+    event.preventDefault();
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/user/${params.userId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('auth_token')}`,
+        },
+      },
+    );
+
+    if (res.ok) {
+      redirect('/dashboard');
+    }
+  }
+
   return (
     <main>
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-2xl font-bold tracking-tight">
           {tenant.data.data?.firstName} {tenant.data.data?.lastName}
         </h2>
+
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <Button variant="destructive">Delete</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                tenant and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={deleteUser}>
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
       <div className="container mx-auto py-10">
         <p>Email: {tenant.data.data?.email}</p>
